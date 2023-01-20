05e0d54fd2f5b23d412a05ff2a5c31f1469996d5
mostafiz9900 <mostafiz9900@gmail.com>
2023-01-20 23:45:33 +0600

    inputfield

--- app/src/main/java/com/example/jettip/MainActivity.kt
+++ app/src/main/java/com/example/jettip/MainActivity.kt
@@ -3,23 +3,32 @@ package com.example.jettip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import com.example.jettip.components.InputField
 import com.example.jettip.ui.theme.JetTipTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,12 +36,14 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp{
-            Text("Alhamdulillah")
+
 //                TopHeader()
+                MainContent()
             }
         }
     }
 }
+
 @Composable
 fun MyApp(content: @Composable () -> Unit){
     JetTipTheme {
@@ -49,18 +60,65 @@ fun MyApp(content: @Composable () -> Unit){
 }
 
 @Composable
-fun  TopHeader(){
+fun  TopHeader(totalPerPerson:Double=0.0){
     Surface(modifier = Modifier
         .fillMaxWidth()
         .height(150.dp)
-        .clip(shape = CircleShape.copy(all = CornerSize(10.dp)))
-        .background(color = MaterialTheme.colorScheme.primary)
+        .clip(shape = CircleShape.copy(all = CornerSize(10.dp))),
+        color = Color(0xFFE9D7F7)
+
 //        .clip(shape = RoundedCornerShape(corner = CornerSize(10.dp)))
+
     ) {
-        Column() {
-            
+        Column(
+            modifier=Modifier.padding(all = 10.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val total="%.2f".format(totalPerPerson)
+            Text(text="Total Per Person", style = MaterialTheme.typography.displayMedium)
+            Text(text="$$total",
+                style = MaterialTheme.typography.displaySmall,
+                fontWeight = FontWeight.Bold
+            )
         }
         
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun MainContent(){
+    val totalBillState= remember {
+        mutableStateOf("")
+    }
+    val validState= remember( totalBillState.value) {
+totalBillState.value.trim().isNotEmpty()
+    }
+    val keyboardController=LocalSoftwareKeyboardController.current
+    Surface(modifier = Modifier
+        .padding(all = 2.dp)
+        .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp),),
+        border = BorderStroke(width = 2.dp, color = Color.LightGray)
+
+    ) {
+       Column() {
+           InputField(
+               valueState = totalBillState,
+               labelId ="Enter Bill" ,
+               enabled = true,
+               isSingleLine =true,
+               onAction = KeyboardActions{
+                   if(!validState) return@KeyboardActions
+                   keyboardController?.hide()
+               }
+           )
+       }
+
+
+
     }
 }
 
@@ -71,7 +129,7 @@ fun  TopHeader(){
 fun DefaultPreview() {
     JetTipTheme {
         MyApp{
-            Text("Alhamdulillah")
+
 
         }
     }
---
+++ app/src/main/java/com/example/jettip/components/Components.kt
@@ -0,0 +1,48 @@
+package com.example.jettip.components
+
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.AttachMoney
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun InputField(
+    modifier: Modifier=Modifier,
+    valueState: MutableState<String>,
+    labelId:String,
+    enabled:Boolean,
+    isSingleLine:Boolean,
+    keyBoardType:KeyboardType= KeyboardType.Number,
+    imeAction:ImeAction=ImeAction.Next,
+    onAction:KeyboardActions=KeyboardActions.Default
+){
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = {
+        valueState.value=it
+    },
+        label = {
+            Text(text = labelId)
+        },
+        leadingIcon = {
+            Icon(imageVector = Icons.Rounded.AttachMoney,
+            contentDescription ="Money Icon" ) },
+        singleLine = isSingleLine,
+        textStyle = TextStyle(fontSize = 18.sp, background = MaterialTheme.colorScheme.onBackground),
+        enabled=enabled,
+        keyboardOptions =   KeyboardOptions(keyboardType =keyBoardType, imeAction = imeAction),
+        keyboardActions = onAction
+    )
+}
